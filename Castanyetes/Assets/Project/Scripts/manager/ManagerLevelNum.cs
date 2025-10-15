28a9b74dce26dabff6568e1616189d19637c211d
Jordi Gratacos <jordi.gratacos@enti.cat>
2025-10-15 20:33:20 +0200

    funciona exepta sumes especifiques

---
+++ Castanyetes/Assets/Project/Scripts/manager/ManagerLevelNum.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ManagerLevel : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
