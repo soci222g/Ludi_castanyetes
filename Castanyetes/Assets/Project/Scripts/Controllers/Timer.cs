84787bf0c9ec8b66e9cd61d89a30bcdf9735bd22
m4rtiop <marti.oliveras@enti.cat>
2025-10-15 23:55:48 +0200

    MainMenu art and more timer and starLogic

--- Castanyetes/Assets/Project/Scripts/Controllers/Timer.cs
+++ Castanyetes/Assets/Project/Scripts/Controllers/Timer.cs
@@ -8,6 +8,12 @@ public class Timer : MonoBehaviour
     [SerializeField] TextMeshProUGUI timerText;
     private float elapsedTime;
 
+    void Start()
+    {
+        elapsedTime = 0f;
+        timerText.text = "00:00";
+    }
+
     void Update()
     {
         elapsedTime += Time.deltaTime;
@@ -15,4 +21,21 @@ public class Timer : MonoBehaviour
         int seconds = Mathf.FloorToInt(elapsedTime % 60f);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public string GetActualTimer()
+    {
+        return timerText.text;
+    }
+    
+
 }
+
