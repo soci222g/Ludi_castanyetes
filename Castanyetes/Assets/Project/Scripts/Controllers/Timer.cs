003dabea6c5350be1527b6d6cb11d757984885d8
m4rtiop <marti.oliveras@enti.cat>
2025-10-23 03:32:42 +0200

    timer enrrera full + ui derrota + boto pause innacabat

--- Castanyetes/Assets/Project/Scripts/Controllers/Timer.cs
+++ Castanyetes/Assets/Project/Scripts/Controllers/Timer.cs
@@ -6,13 +6,14 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI timerText;
+        [SerializeField] private GameObject win;
     private float elapsedTime;
 
     void Start()
     {
         Resume();
-        elapsedTime = 120f;
-        timerText.text = "02:00";
+        elapsedTime = 150f;
+        timerText.text = "02:30";
     }
 
     void Update()
@@ -21,12 +22,19 @@ public class Timer : MonoBehaviour
         int minutes = Mathf.FloorToInt(elapsedTime / 60f);
         int seconds = Mathf.FloorToInt(elapsedTime % 60f);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        if (timerText.text == "00:00")
+        {
+            win.SetActive(true);
+            win.GetComponent<StarsController>().LevelFailed();
+        }
     }
 
     public void Pause()
     {
         Time.timeScale = 0f;
     }
+
     public void Resume()
     {
         Time.timeScale = 1f;
