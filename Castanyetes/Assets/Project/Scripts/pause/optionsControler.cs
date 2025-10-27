7d58cad9a9efb1c9b8367a994b44a9b8385bcdd0
jordi gratacos <Jordi.gratacos@estuidiant.enti.cat>
2025-10-27 17:41:53 +0100

    SlidersFets

---
+++ Castanyetes/Assets/Project/Scripts/pause/optionsControler.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class optionsControler : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioMixer audioMixer;
+    [SerializeField]
+    private string grup;
+    private Slider slider;
+    [SerializeField]
+ 
+
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+
+    }
+
+  
+    public void SetVolume()
+    {
+        audioMixer.SetFloat(grup, Mathf.Log10(slider.value) * 80f);
+    }
+}
